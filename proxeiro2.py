a7696eb343a752c29929124fc03fc153a91cec42
wehappyfew <wehappyfew@live.com>
2016-01-26 10:40:54 +0200

    changes

--- proxeiro2.py
+++ proxeiro2.py
@@ -2,7 +2,7 @@ __author__ = 'wehappyfew'
 
 import subprocess,os
 
-# 1csdcsddvd
+#
 # p = subprocess.Popen( "ipconfig", stdout=subprocess.PIPE, shell=True )
 # (output, err) = p.communicate()
 # print output, err
